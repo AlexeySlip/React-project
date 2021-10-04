5690dff504bb6162c8711a2fd0bf6dc55cf9e591
Alexey <Aleksey.Slipchenko97@gmail.com>
2021-10-04 13:41:53 +0300

    added prettierrc and CssBaseline

---
+++ src/container/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Header from './Header/Header'
+import Main from './Main/Main'
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+        </>
+    )
+}
+
+export default App
---
+++ src/container/Header/Header.js
@@ -0,0 +1,14 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <Container>
+                <h1>Hello World</h1>
+            </Container>
+        </>
+    )
+}
+
+export default Header
